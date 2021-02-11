7cb38e652dab6ea66c275926010562f2dda8e9c9
RobertCB87 <69755654+robertCB87@users.noreply.github.com>
2021-02-12 00:29:41 +0100

    refactored js code

--- resources/js/calculator.js
+++ resources/js/calculator.js
@@ -9,39 +9,37 @@ buttons.forEach( button => {
 })
 
 const calculator = (value) => {
-    console.log(value);
-
+    // needed for check the length
     const displayStr = String(currentResult.innerHTML);
-    console.log(displayStr);
 
+    // check for number-buttons
     if(!isNaN(value)) {
         if((displayStr.length === 1) && (displayStr === "0") ) {
-            console.log("AND");
             currentResult.innerHTML = value;
         } else {
-            console.log("ADD");
             currentResult.innerHTML += value;
         }
-
+    // check for comma-button
     } else if (value === ",") {
-
-        currentResult.innerHTML += value;
-
+        if (!currentResult.innerHTML.includes(",")) {
+            currentResult.innerHTML += value;
+        }  
+    // check for functions-buttons
     } else {
 
         switch (value) {
-            case (Number(value)):
-                currentResult.innerHTML =+ value;
-                break;
             case 'clearAll':
                 clearAll(value);
                 break;
             case 'del':
                 deleteChar();
                 break;
+            case 'change':
+                alterSign();
+                break;
             default:
                 return;
-      }
+        }
     }
     
 }
@@ -66,6 +64,10 @@ const addComma = (value) => {
     return value + ",";
 }
 
+const clearCurrentResult = () => {
+    currentResult.innerHTML = 0;
+}
+
 const clearAll = () => {
     formerInput.innerHTML = "";
     currentResult.innerHTML = "0";
@@ -75,26 +77,24 @@ const deleteChar = () => {
     let displayStr = currentResult.innerHTML;
 
     if(displayStr !== "0") {
-        displayStr = displayStr.substring(0, displayStr.length-1);
-        if(displayStr.length === 0) {
+        const newStr = displayStr.substring(0, displayStr.length-1);
+        if(newStr.length === 0) {
             currentResult.innerHTML = "0";
         } else {
-        currentResult.innerHTML = displayStr;
+        currentResult.innerHTML = newStr;
         }
     } else {
-        console.log("else")
         currentResult.innerHTML = "0";
     }
 }
 
+ /* parseFloat doesn't works with comma (,), so we need to replace it with a point (.) and vice versa */
 const alterSign = () => {
-    const displayStr = currentResult.innerHTML;
-    const displayLength = displayStr.length;
+    const displayStr = currentResult.innerHTML
 
-    if(displayStr[displayLength - 1] === ",") {
-        // deleteChar erfordert kein Argument mehr 
-        return String(Number(deleteChar(displayStr)) * (-1)).concat(",");
-    }
-    
-    return Number(displayStr) * (-1);
+    const alteredFloat = parseFloat(displayStr.replace(',', '.')) * -1;
+
+    const alteredStr = String(alteredFloat).replace('.', ',');
+
+    currentResult.innerHTML = alteredStr;
 }
