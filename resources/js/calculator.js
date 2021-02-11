bba6d4b876cef1a7c76c85272804df4836a5e5df
RobertCB87 <69755654+robertCB87@users.noreply.github.com>
2021-02-11 01:45:22 +0100

    new lines of js

--- resources/js/calculator.js
+++ resources/js/calculator.js
@@ -1,4 +1,6 @@
 const buttons = document.querySelectorAll("button");
+const currentResult = document.getElementById("currentResult");
+const formerInput = document.getElementById("formerInput");
 
 buttons.forEach( button => {
     button.addEventListener("click", input => {
@@ -26,8 +28,24 @@ const addComma = (value) => {
     return value + ",";
 }
 
+const clearAll = () => {
+    formerInput.innerHTML = "";
+    currentResult.innerHTML = "0";
+}
+
 const deleteChar = (value) => {
     if(value) {
        return String(value).substring(0, value.length-1); 
     }return;
+}
+
+const change = (value) => {
+    const valueStr = String(value);
+    const lengthStr = valueStr.length;
+
+    if(valueStr[lengthStr - 1] === ",") {
+        return String(Number(deleteChar(valueStr)) * (-1)).concat(",");
+    }
+    
+    return Number(valueStr) * (-1);
 }
